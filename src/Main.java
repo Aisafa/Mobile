566229ba2414478e5fea7a4e258e71c24245fe84
Aidar <Agsafiullin@gmail.com>
2020-08-27 23:28:45 +0300

    add gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         long balance = 100;
-        int replenishment = 1010;
-        long bonusPercent = 1;
+        int replenishment = 2100;
         long totalBonus;
-        long totalBalance;
         if (replenishment < 1000) {
-            System.out.print("Your balance = ");
-            totalBalance = balance + replenishment;
-            System.out.println(totalBalance);
-        } else {
-            totalBonus = replenishment / 100 * bonusPercent;
-            totalBalance = balance + replenishment + totalBonus;
-            System.out.print("Your bonus = ");
-            System.out.println(totalBonus);
-            System.out.print("Your balance = ");
-            System.out.println(totalBalance);
+            System.out.print("Your balance =  0 ");
         }
+        else {
+            totalBonus = replenishment / 100;
+            System.out.println("Your bonus = " + totalBonus);
+                    }
 
     }
 }
