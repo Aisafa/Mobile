1e586b26d736c670c6c2905217b8b157b946c91c
Aidar <Agsafiullin@gmail.com>
2020-08-27 18:11:59 +0300

    change bonus

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        long balance = 500;
-        int replenishment = 1000;
-        long bonusPercent = 3;
-        long totalBonus = replenishment/100*bonusPercent;
-        long totalBalance = balance+replenishment+totalBonus;
-        System.out.print("Your bonus = ");
-        System.out.println(totalBonus);
-        System.out.print("Your balance = ");
-        System.out.println(totalBalance);
+        long balance = 100;
+        int replenishment = 1010;
+        long bonusPercent = 1;
+        long totalBonus;
+        long totalBalance;
+
+
+        if (replenishment < 1000) {
+            System.out.print("Your balance = ");
+            totalBalance = balance + replenishment;
+            System.out.println(totalBalance);
+        } else {
+            totalBonus = replenishment / 100 * bonusPercent;
+            totalBalance = balance + replenishment + totalBonus;
+            System.out.print("Your bonus = ");
+            System.out.println(totalBonus);
+            System.out.print("Your balance = ");
+            System.out.println(totalBalance);
+        }
+
     }
 }
